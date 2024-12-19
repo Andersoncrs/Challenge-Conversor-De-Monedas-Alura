7ab420ba7f5a9f4b2888d3e6627043c2b8edfc0d
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2024-12-19 17:49:25 -0500

    Creacion de metdod para onbtener un JSON de ConversorMoneda de la clase ConversorJson

--- src/com/aluracursos/conversordemonedas/conversordeformatos/ConversorJson.java
+++ src/com/aluracursos/conversordemonedas/conversordeformatos/ConversorJson.java
@@ -1,17 +1,23 @@
 package com.aluracursos.conversordemonedas.conversordeformatos;
 
 import com.aluracursos.conversordemonedas.dtos.ConversionMonedaExchangerate;
+import com.aluracursos.conversordemonedas.modelos.ListaMonedasConsultadas;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class ConversorJson {
-    private final String json;
+    private  String json;
+    private ListaMonedasConsultadas listaMonedasConsultadas;
 
     Gson gson = new GsonBuilder()
             .setPrettyPrinting()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .create();
+
+    public ConversorJson(ListaMonedasConsultadas listaMonedasConsultadas) {
+        this.listaMonedasConsultadas = listaMonedasConsultadas;
+    }
 
     public ConversorJson(String json ){
         this.json = json;
@@ -20,4 +26,9 @@ public class ConversorJson {
     public ConversionMonedaExchangerate convertirAMonedaExchangerate(){
         return gson.fromJson(this.json, ConversionMonedaExchangerate.class);
     }
+
+    public String ConvertirListaDeMonedasAJson(){
+        return gson.toJson(this.listaMonedasConsultadas);
+    }
+
 }
--- src/com/aluracursos/conversordemonedas/principal/Main.java
+++ src/com/aluracursos/conversordemonedas/principal/Main.java
@@ -30,8 +30,10 @@ public class Main {
                     if (listaMonedasConsultadas.validarElementosLista()) {
                         continue;
                     }
-                    //Logica de guardar Archivo
-                    break;
+                    String jsonConversionesRealizadas = new ConversorJson(listaMonedasConsultadas).
+                            ConvertirListaDeMonedasAJson();
+                    System.out.println(jsonConversionesRealizadas);
+                    continue;
             }
             String eleccionResultadoMonedaObjetivo = menu.mostrarMenuObjetivo(eleccionResultadoMonedaBase);
 
