5ff75c1a7e60d80f5a8227fa8262a008ce31c2eb
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2024-12-18 20:35:14 -0500

    Añade metodo mostrarBienvenida de la clase Menu y se utiliza en la clase Main

--- src/com/aluracursos/conversordemonedas/modelos/Menu.java
+++ src/com/aluracursos/conversordemonedas/modelos/Menu.java
@@ -1,4 +1,12 @@
 package com.aluracursos.conversordemonedas.modelos;
 
 public class Menu {
+
+    public void mostrarBienvenida(){
+        System.out.println("""
+                ********************************************************************************
+                ***********    Bienvenido al Conversor de Monedas WorldChangeRate    ***********
+                ********************************************************************************
+                Aquí podrás hallar la tasa de conversion de multiples monedas a nivel mundial""");
+    }
 }
--- src/com/aluracursos/conversordemonedas/principal/Main.java
+++ src/com/aluracursos/conversordemonedas/principal/Main.java
@@ -1,4 +1,12 @@
 package com.aluracursos.conversordemonedas.principal;
 
+import com.aluracursos.conversordemonedas.modelos.Menu;
+
 public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+
+        menu.mostrarBienvenida();
+
+    }
 }
