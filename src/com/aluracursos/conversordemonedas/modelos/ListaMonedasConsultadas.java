cb62c260cfff3089952c3d94b5c1c81026a9bb80
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2024-12-19 16:33:22 -0500

    Implementacion de modularidad de metodos de la clase ListaMonedasConsultadas

--- src/com/aluracursos/conversordemonedas/modelos/ListaMonedasConsultadas.java
+++ src/com/aluracursos/conversordemonedas/modelos/ListaMonedasConsultadas.java
@@ -6,15 +6,12 @@ import java.util.List;
 public class ListaMonedasConsultadas {
     private List<ConversorMoneda> listaConversionMonedas = new ArrayList<>();
 
-    public void agregarConversionMoneda(ConversorMoneda conversorMoneda){
+    public void agregarConversionMoneda(ConversorMoneda conversorMoneda) {
         listaConversionMonedas.add(conversorMoneda);
     }
 
-    public void mostrarMonedasConvertidas(){
-        if(this.listaConversionMonedas.isEmpty()){
-            System.out.println("\n********************************************************************************");
-            System.out.println("El historial esta vacio. No ha realizado ninguna conversion en esta sesion");
-            System.out.println("********************************************************************************\n");
+    public void mostrarMonedasConvertidas() {
+        if (validarElementosLista()) {
             return;
         }
         System.out.println("\n********************************************************************************");
@@ -27,5 +24,18 @@ public class ListaMonedasConsultadas {
         System.out.println("************************** FIN HISTORIAL CONVERSIONES **************************");
         System.out.println("********************************************************************************\n");
     }
-}
 
+    public boolean validarElementosLista() {
+        if(this.listaConversionMonedas.isEmpty()){
+            mostrarMensjeListaVacia();
+            return true;
+        }
+        return false;
+    }
+
+    public void mostrarMensjeListaVacia(){
+        System.out.println("\n********************************************************************************");
+        System.out.println("El historial esta vacio. No ha realizado ninguna conversion en esta sesion");
+        System.out.println("********************************************************************************\n");
+    }
+}
